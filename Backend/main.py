67350f880b0da953cb181f6886a7e026aecad1b5
Ram Gopal Reddy <redabothularamgopalreddy@gmail.com>
2026-02-22 11:05:13 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -39,7 +39,7 @@ SECRET_KEY = "supersecretkey123"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # ==========================================================
